b81dd44824cef759b989da62d60de873a43d6992
Sergey Pevnev <larockarocka@hotmail.com>
2017-10-17 11:46:30 +0500

    Set version on TC build

---
+++ .files/SetVersionInfo.ps1
@@ -0,0 +1,20 @@
+param
+(
+  [Parameter(HelpMessage = "Build number.")]
+  [int] $BuildNumber = 0,
+
+  [Parameter(HelpMessage = "Commit.")]
+  [string] $Commit = 'null',
+
+  [Parameter(HelpMessage = "Path to the Infinni.Deployer.csproj.")]
+  [string] $ProjectPath = '..\Infinni.Deployer\Infinni.Deployer.csproj'
+)
+
+[xml] $csproj = Get-Content $ProjectPath
+
+foreach ($group in $csproj.Project.PropertyGroup | Where-Object {$_.AssemblyVersion -ne $null}) {
+  $group.AssemblyVersion = "$($group.Version).$BuildNumber"
+  $group.FileVersion = "$($group.Version).$BuildNumber"
+}
+
+$csproj.Save($ProjectPath)
